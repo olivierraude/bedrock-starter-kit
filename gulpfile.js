4f9585e2a382147bbcfd11374dbea49a2383fb9e
Olivier Raude <olivierraude@gmail.com>
2025-05-12 16:06:02 -0400

    cleanup gulpfile plugins

--- gulpfile.js
+++ gulpfile.js
@@ -8,35 +8,40 @@
   source = "web/app/themes/your_theme_name/";
   proxy = "http://localhost/your_file_name/web/";
   
-  const {src,dest,watch,series} = require("gulp");
-  const sass = require("gulp-sass")(require("sass"));
+  
+  // const {src,dest,watch,series} = require("gulp");
+  const gulp = require('gulp');
+  const sass = require('gulp-sass')(require('sass'));
   const sourcemaps = require('gulp-sourcemaps');
   const plumber = require('gulp-plumber');
-  const postcss = require("gulp-postcss");
-  const autoprefixer = require("gulp-autoprefixer");
+  const postcss = require('gulp-postcss');
+  const autoprefixer = require('autoprefixer');
   const cleanCSS = require('gulp-clean-css');
-  const rename = require("gulp-rename");
-  const uglify = require("gulp-uglify");
+  const rename = require('gulp-rename');
+  const uglify = require('gulp-uglify');
   const browserSync = require("browser-sync").create();
-
-// Sass Task
-function scssTask() {
-  return gulp.src(source + 'scss/**/*.scss')
-    .pipe(plumber())
-    .pipe(sourcemaps.init())
-    .pipe(sass.sync().on('error', sass.logError))
-    .pipe(postcss([autoprefixer()])) // <- autoprefixer utilisé ici
-    .pipe(cleanCSS({ compatibility: 'ie11' }))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('web/app/themes/your-theme-name/dist/css'));
-}
+  
+  // Tâche SCSS
+  function scssTask() {
+    return gulp.src(source + 'sass/**/*scss')
+      .pipe(plumber())
+      .pipe(sourcemaps.init())
+      .pipe(sass.sync().on('error', sass.logError))
+      // .pipe(postcss([autoprefixer({cascade: false})]))
+      .pipe(postcss([autoprefixer()]))
+      .pipe(gulp.dest(source + 'sass')) // ← adapte ce chemin
+      .pipe(cleanCSS({ compatibility: 'ie11' }))
+      .pipe(rename({ suffix: ".min" }))
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest(source + 'dist/css')); // ← adapte ce chemin
+  }
 
 // Script Task
 function scriptTask() {
-  return src(source + "js/**/*.js", { sourcemaps: true })
+  return gulp.src(source + "js/**/*.js", { sourcemaps: true })
     .pipe(uglify())
     .pipe(rename({ suffix: ".min" }))
-    .pipe(dest(source + "dist/js/"));
+    .pipe(gulp.dest(source + "dist/js"));
 };
 
 // Images Task
@@ -56,15 +61,15 @@ function browsersyncReload(cb) {
 
 // Watch Task
 function watchTask() {
-  watch(source + "**/*php", browsersyncReload);
-  watch(
+  gulp.watch(source + "**/*php", browsersyncReload);
+  gulp.watch(
     [source + "sass/**/*scss", source + "js/*js"],
-    series(scssTask, scriptTask, browsersyncReload)
+    gulp.series(scssTask, scriptTask, browsersyncReload)
   );
 }
 
 // Default Gulp Task
-exports.default = series(
+exports.default = gulp.series(
   scssTask,
   scriptTask,
   browsersyncServe,
